f03945d88da31c2eadfcfc4fd074fdc72aa85af3
git <marlonergondossantos@gmail.com>
2019-10-02 12:45:59 -0300

    Add Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 import GlobalStyle from "./style/globalStyle";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import Routes from "./routes";
+import Header from "./components/Header/index";
 
 export default function app() {
   return (
     <>
       <GlobalStyle />
       <Router>
+        <Header />
         <Routes />
       </Router>
     </>
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+
+import React from "react";
+
+export default function header() {
+  return <Header />;
+}

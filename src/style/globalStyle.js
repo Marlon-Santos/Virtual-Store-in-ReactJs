7af0cdf8be533578cfedc1a6a681749578bfefdc
git <marlonergondossantos@gmail.com>
2019-10-02 10:25:13 -0300

    Global Styles

---
+++ src/style/globalStyle.js
@@ -0,0 +1,19 @@
+import backg from "../assets/background.svg";
+
+import { createGlobalStyle } from "styled-components";
+export default createGlobalStyle`
+    *{
+        margin:0;
+        border:0;
+        box-sizing:border-box;
+    }
+    html,body,#root{
+        height: 100%;
+
+    }
+    body{
+        background: #191920 url(${backg}) no-repeat;
+        background-size:contain;
+        color:white
+    }
+`;

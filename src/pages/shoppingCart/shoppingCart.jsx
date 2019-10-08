2877c015fccf957f5a1f04d9ad45e6a209fada6a
git <marlonergondossantos@gmail.com>
2019-10-08 12:17:44 -0300

    removed calc css

--- src/pages/shoppingCart/shoppingCart.jsx
+++ src/pages/shoppingCart/shoppingCart.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import {
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+  MdDelete
+} from "react-icons/md";
+
 import { Form } from "./style";
 
 export default function cart() {
   return (
     <>
       <Form>
-        <div>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-flex-2018-rn-feminino/26/D12-9472-026/D12-9472-026_zoom1.jpg"
-            alt="tenis"
-          />
-        </div>
         <table>
           <thead>
             <tr>
+              <th />
               <th>produto</th>
               <th>qtd</th>
               <th>subtotal</th>
@@ -22,21 +23,38 @@ export default function cart() {
           <tbody>
             <tr>
               <td>
-                tenis muito massa <div>R$ 129,90</div>
+                <div>
+                  <img
+                    src="https://static.netshoes.com.br/produtos/tenis-nike-wmns-flex-2018-rn-feminino/26/D12-9472-026/D12-9472-026_zoom1.jpg"
+                    alt="tenis"
+                  />
+                </div>
               </td>
               <td>
-                <input type="number" />
+                <div>
+                  Tênis muito massa <strong>R$ 129,90</strong>
+                </div>
               </td>
+              <td>
+                <div>
+                  <MdAddCircleOutline color={"purple"} size={20} />
+                  <input type="number" readOnly value="2" name="qtd" />
+                  <MdRemoveCircleOutline color={"purple"} size={20} />
+                </div>
+              </td>
+
               <td>R$ 129,90</td>
-              <td>Lixeira</td>
+              <td>
+                <MdDelete color={"purple"} size={20} />
+              </td>
             </tr>
           </tbody>
         </table>
         <footer>
           <input type="submit" value="finalizar pedido" />
           <div>
-            <span>total</span>
-            <span>R$1920,28</span>
+            <span>total:</span>
+            <strong>R$1920,28</strong>
           </div>
         </footer>
       </Form>
--- src/pages/shoppingCart/style.js
+++ src/pages/shoppingCart/style.js
@@ -12,22 +12,37 @@ export const Form = styled.form`
   padding: 25px;
   align-items: flex-start;
   table {
-    text-align: center;
-    width: calc(100% - 200px);
-    th {
-      padding-bottom: 60px;
-    }
-    th {
-      color: #999;
-      margin-bottom: 50px;
-    }
-  }
-  > div {
-    width: 200px;
-    background-color: red;
-  }
-  input {
+    text-align: left;
+    vertical-align: top;
     width: 100%;
+  }
+  td:nth-child(1) > div {
+    width: 200px;
+  }
+  th {
+    color: #aaa;
+  }
+  td:nth-child(2) {
+    div {
+      display: inline-block;
+      strong {
+        display: block;
+      }
+    }
+  }
+  td:nth-child(3) {
+    div {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+    }
+    input {
+      width: 35px;
+      text-align: right;
+      display: inline-block;
+      border: 1px solid #ddd;
+      height: 25px;
+    }
   }
   img {
     width: 100%;
@@ -36,5 +51,26 @@ export const Form = styled.form`
     display: flex;
     flex: 1;
     justify-content: space-between;
+    align-items: center;
+    input {
+      appearance: none;
+      height: 50px;
+      width: 150px;
+      background-color: #551a8b;
+      border: 2px solid;
+      border-radius: 15px;
+      color: white;
+      font-size: 16px;
+      margin-left: 25px;
+    }
+    div {
+      margin-right: 25px;
+    }
+    span {
+      font-size: 16px;
+    }
+    strong {
+      font-size: 22px;
+    }
   }
 `;
